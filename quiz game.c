b441dcc491354a86149db044b7f642494229d23e
Alex Tamang <at661839@gmail.com>
2026-03-13 22:54:46 +0545

    Update quiz game.c

--- quiz game.c
+++ quiz game.c
@@ -1,6 +1,23 @@
 //program to make a quiz game
 #include<stdio.h>
-int mian(){
-	char que,ans;
-	printf("what is the full form of HTML");
+int main(){
+	int i,ans;
+	printf("What is the full form of HTML");
+	printf("\n1.Hyper Text Markup language");
+	printf("\n2.Hyper transfer Markup language");
+	printf("\n3.Hyper Text message logic");
+	printf("\n4.Hyper Text Markup language");
+	printf("\nWhich number answer is correct:");
+	scanf("%d",&i);
+	if(i>1){
+		printf("The answer is incorrect");
+	}
+	if(1==i){
+		printf("The answer is correct");
+	}
+	if(i>4){
+		printf("\nIn this question there will olny 4 answer not 5");
+	}
+	
+	return 0;
 }
